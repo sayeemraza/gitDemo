a1c3e5f07415446116771792b0ad742ba23fe4ba
sayeemraza <68975896+sayeemraza@users.noreply.github.com>
2020-08-01 16:03:16 -0400

    Update bookARoom.java

--- src/bookARoom.java
+++ src/bookARoom.java
@@ -9,7 +9,21 @@ public class bookARoom {
 // push back to intellij 
         // i added this line from github
 
+/*
+PS
+git init
+git add . 
+git commit -m”add HelloB20.java” or update
+git remote add origin URL (If it says remote origin already exists.) then type below
+git remote set-url origin URL
+git push -u origin master
+git push
+:100:
+9
+:medal:
+5
 
+*/
 
 
 
